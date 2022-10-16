1cc39c49e5fe67dd6dbf88db4f6606cb43bbcd1f
ROMAN <roman90x@icloud.com>
2022-10-17 01:08:52 +0600

    решение задачи из билета

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private final String author;
+    private final String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
